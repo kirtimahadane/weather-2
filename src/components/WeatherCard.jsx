dbf9ad6f001506835fa9329c4c2ebf78c51a1d3b
kirtimahadane <kirti.mahadane@gmail.com>
2024-06-06 11:47:07 +0530

    env file added

---
+++ src/components/WeatherCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const WeatherCard = ({ title, data }) => {
+  
+  return (
+    <div className="weather-card">
+      <h3>{title}</h3>
+      <p>{data}</p>
+    </div>
+  );
+};
+export default WeatherCard;
